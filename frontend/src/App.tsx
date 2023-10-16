8c3d7f50936277675f2641e71af91435e4e48670
0xNino <86327707+0xNino@users.noreply.github.com>
2023-10-17 00:11:08 +0200

    add prettierignore file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,11 +13,7 @@ function App() {
 					<img src={viteLogo} className="logo" alt="Vite logo" />
 				</a>
 				<a href="https://react.dev" target="_blank">
-					<img
-						src={reactLogo}
-						className="logo react"
-						alt="React logo"
-					/>
+					<img src={reactLogo} className="logo react" alt="React logo" />
 				</a>
 			</div>
 			<h1>Vite + React</h1>
