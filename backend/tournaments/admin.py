a8b0383f7717d6dac15248fd23a5793d9ba4f242
0xNino <86327707+0xNino@users.noreply.github.com>
2023-11-14 10:09:16 +0100

    init tournaments models and apply migrations

--- backend/tournaments/admin.py
+++ backend/tournaments/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-from .models import Tournament, TournamentParticipant
+from .models import Tournament # TournamentParticipant
 
 @admin.register(Tournament)
 class TournamentAdmin(admin.ModelAdmin):
     list_display = ('name', 'start_date', 'end_date')
     search_fields = ('name',)
 
-@admin.register(TournamentParticipant)
-class TournamentParticipantAdmin(admin.ModelAdmin):
-    list_display = ('user', 'tournament', 'joined_at')
-    search_fields = ('user__username', 'tournament__name')
+# @admin.register(TournamentParticipant)
+# class TournamentParticipantAdmin(admin.ModelAdmin):
+#     list_display = ('user', 'tournament', 'joined_at')
+#     search_fields = ('user__username', 'tournament__name')
--- backend/tournaments/models.py
+++ backend/tournaments/models.py
@@ -1,23 +1,37 @@
-# tournaments/models.py
 from django.db import models
-from users.models import CustomUser
+from django.conf import settings
+
+class PlayerProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    elo_rating = models.IntegerField(default=1000)  # ELO rating for each player
 
 class Tournament(models.Model):
+    SINGLE_ELIMINATION = 'SE'
+    ROUND_ROBIN = 'RR'
+
+    TOURNAMENT_TYPES = [
+        (SINGLE_ELIMINATION, 'Single Elimination'),
+        (ROUND_ROBIN, 'Round Robin'),
+    ]
+
     name = models.CharField(max_length=255)
     start_date = models.DateTimeField()
-    end_date = models.DateTimeField(null=True, blank=True)
-    participants = models.ManyToManyField(CustomUser, through='TournamentParticipant')
+    end_date = models.DateTimeField()
+    tournament_type = models.CharField(max_length=2, choices=TOURNAMENT_TYPES, default=SINGLE_ELIMINATION)
 
-    def __str__(self):
-        return self.name
+class Match(models.Model):
+    tournament = models.ForeignKey(Tournament, related_name='matches', on_delete=models.CASCADE)
+    player1 = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='matches_as_player1', on_delete=models.CASCADE)
+    player2 = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='matches_as_player2', on_delete=models.CASCADE)
+    winner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='won_matches', on_delete=models.SET_NULL, null=True, blank=True)
+    match_date = models.DateTimeField()
 
-class TournamentParticipant(models.Model):
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE)
-    joined_at = models.DateTimeField(auto_now_add=True)
+class Round(models.Model):
+    tournament = models.ForeignKey(Tournament, related_name='rounds', on_delete=models.CASCADE)
+    round_number = models.IntegerField()
+    matches = models.ManyToManyField(Match)
 
-    class Meta:
-        unique_together = ('user', 'tournament')  # Each user can only join a tournament once
-
-    def __str__(self):
-        return f"{self.user} in {self.tournament}"
+class MatchmakingQueue(models.Model):
+    player = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    # Additional fields like 'preferred_tournament_type' can be added
