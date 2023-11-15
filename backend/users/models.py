2ff4eaf2d4279c784e46e65cf587be2e74512ede
0xNino <86327707+0xNino@users.noreply.github.com>
2023-11-16 00:53:13 +0100

    make display_name optional

--- backend/users/models.py
+++ backend/users/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    display_name = models.CharField(max_length=100, unique=True)
+    display_name = models.CharField(max_length=100, unique=True, blank=True, null=True)
 
     def __str__(self):
         return self.username
