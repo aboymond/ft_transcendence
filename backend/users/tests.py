cd17da5a27d3059732837ee678aace53113ca5ec
0xNino <86327707+0xNino@users.noreply.github.com>
2023-11-16 12:36:13 +0100

    add tests for game history

--- backend/users/tests.py
+++ backend/users/tests.py
@@ -1,7 +1,10 @@
-from rest_framework.test import APITestCase
+from django.test import TestCase
 from django.urls import reverse
-from rest_framework import status
 from django.contrib.auth import get_user_model
+from rest_framework.test import APITestCase
+from rest_framework.test import APIClient
+from rest_framework import status
+from .models import GameHistory
 
 User = get_user_model()
 
@@ -42,3 +45,35 @@ class UserUpdateViewTest(APITestCase):
 
         # Check that the response status code is 401 Unauthorized
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+class GameHistoryTest(TestCase):
+    def setUp(self):
+        # Create test users
+        self.user1 = User.objects.create_user(username='user1', password='password123')
+        self.user2 = User.objects.create_user(username='user2', password='password123')
+        
+        # Create a game history instance
+        self.game_history = GameHistory.objects.create(winner=self.user1)
+        self.game_history.players.add(self.user1, self.user2)
+
+        # Set up the API client
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user1)
+
+    def test_retrieve_game_history(self):
+        # Get the URL for retrieving game history
+        url = reverse('game-history-list')
+
+        # Make the request
+        response = self.client.get(url)
+
+        # Test that the response is 200 OK
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # Test that the response contains the game history
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['winner'], self.user1.id)
+
+    # def test_game_history_content(self):
+    #     # Additional tests to verify the content of the game history
+    #     # Check if the game history contains correct player information, etc.
