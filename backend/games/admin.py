5fecd2a6f5b8bec00f0e55052f2f9ea9fe421d3d
0xNino <86327707+0xNino@users.noreply.github.com>
2023-11-16 23:42:05 +0100

    refactor game model

--- backend/games/admin.py
+++ backend/games/admin.py
@@ -1,7 +1,4 @@
 from django.contrib import admin
 from .models import Game
 
-@admin.register(Game)
-class GameAdmin(admin.ModelAdmin):
-    list_display = ('player1', 'player2', 'start_time', 'end_time', 'winner')
-    search_fields = ('player1__username', 'player2__username')
+admin.site.register(Game)
--- backend/games/models.py
+++ backend/games/models.py
@@ -1,14 +1,21 @@
 from django.db import models
-from users.models import CustomUser
+from django.conf import settings
 
 class Game(models.Model):
-    player1 = models.ForeignKey(CustomUser, related_name='games_as_player1', on_delete=models.CASCADE)
-    player2 = models.ForeignKey(CustomUser, related_name='games_as_player2', on_delete=models.CASCADE)
+    STATUS_CHOICES = [
+        ('waiting', 'Waiting for Player'),
+        ('in_progress', 'In Progress'),
+        ('completed', 'Completed'),
+    ]
+
+    player1 = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='games_as_player1', on_delete=models.SET_NULL, null=True, blank=True)
+    player2 = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='games_as_player2', on_delete=models.SET_NULL, null=True, blank=True)
+    status = models.CharField(max_length=15, choices=STATUS_CHOICES, default='waiting')
     start_time = models.DateTimeField(auto_now_add=True)
     end_time = models.DateTimeField(null=True, blank=True)
-    winner = models.ForeignKey(CustomUser, related_name='games_won', on_delete=models.SET_NULL, null=True, blank=True)
-    # Game state JSON or a more complex state can be stored here depending on the game mechanics
-    game_state = models.JSONField(null=True, blank=True)
+    # winner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='games_won', on_delete=models.SET_NULL, null=True, blank=True)
+    # Add additional fields as needed, like scores, etc.
 
-    def __str__(self):
-        return f"{self.player1} vs {self.player2}"
+    @property
+    def is_full(self):
+        return self.player1 is not None and self.player2 is not None
