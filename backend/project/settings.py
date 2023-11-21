d1055c44de2ad9f1dd8eed6a5e7dd0ab6eb67fff
0xNino <86327707+0xNino@users.noreply.github.com>
2023-11-22 00:11:02 +0100

    update asgi config

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -85,10 +85,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 ASGI_APPLICATION = 'project.routing.application'
 
-# Using in-memory channel layer
 CHANNEL_LAYERS = {
     'default': {
-        'BACKEND': 'channels.layers.InMemoryChannelLayer',
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
     },
 }
 
