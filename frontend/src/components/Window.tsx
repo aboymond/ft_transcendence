c8764b878bcad812df59e14d3c82e137dd17f216
Gab <gab@MacBook-Pro-de-Anon.local>
2023-12-15 16:49:36 +0100

    display history on window

--- frontend/src/components/Window.tsx
+++ frontend/src/components/Window.tsx
@@ -5,29 +5,66 @@ import GameHistoryList from './GameHistoryList';
 
 const Window: React.FC = () => {
     const [showHistory, setShowHistory] = useState(false);
+    const [showFriends, setShowFriends] = useState(false);
 
     return (
         <Container className={styles.window}>
-            <Row>
-                <Col> 
-                    <Button 
-                        variant="primary"
-                        type="submit"
-						className={styles.button}
-                        onClick={() => setShowHistory(!showHistory)}
-                    >
-                        History
-                    </Button>
-                    {showHistory && <GameHistoryList />}
-                </Col>
-                <Col>
-                    <Button 
-                    variant="primary"
-                    type="submit"
-					className={styles.button}
-                    >Friends
-                    </Button>
-                </Col>
+            <Row className={styles.row}>
+                {!showHistory && !showFriends && (
+                    <>
+                        <Col> 
+                            <Button 
+                                variant="primary"
+                                type="submit"
+                                className={styles.button}
+                                onClick={() => {
+                                    setShowHistory(true);
+                                    setShowFriends(false);
+                                }}
+                            >
+                                History
+                            </Button>
+                        </Col>
+                        <Col>
+                            <Button 
+                            variant="primary"
+                            type="submit"
+                            className={styles.button}
+                            onClick={() => {
+                                setShowFriends(true);
+                                setShowHistory(false);
+                            }}
+                            >Friends
+                            </Button>
+                        </Col>
+                    </>
+                )}
+                {showHistory && (
+                    <Col className={styles.fullWindowContent}>
+                        <GameHistoryList />
+                        <Button 
+                            variant="primary"
+                            type="submit"
+                            className={`${styles.button} ${styles.closeButton}`}
+                            onClick={() => setShowHistory(false)}
+                        >
+                            X
+                        </Button>
+                    </Col>
+                )}
+                {showFriends && (
+                    <Col className={styles.fullWindowContent}>
+                        <div>Friends List</div>
+                        <Button 
+                            variant="primary"
+                            type="submit"
+                            className={`${styles.button} ${styles.closeButton}`}
+                            onClick={() => setShowFriends(false)}
+                        >
+                            X
+                        </Button>
+                    </Col>
+                )}
             </Row>
         </Container>
     );
